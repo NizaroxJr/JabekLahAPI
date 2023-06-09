56e50f7560f09a61883f71eb09460617721071b1
unknown <nizar.elhilalii@gmail.com>
2023-06-09 11:40:52 +0100

    Last Commit To Deploy yy

--- src/main/java/com/example/jabeklah/security/CorsConfiguration.java
+++ src/main/java/com/example/jabeklah/security/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.jabeklah.security;
+/*package com.example.jabeklah.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +9,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Match all paths
-                .allowedOrigins("http://localhost:4200", "http://localhost:4201", "http://localhost:4202") // Add your client URLs here
+                .allowedOrigins("https://legendary-dragon-83c451.netlify.app/", "https://frolicking-cheesecake-ca3b24.netlify.app/", "https://whimsical-peony-04b1a2.netlify.app/","https://classy-begonia-fd3a71.netlify.app/") // Add your client URLs here
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the HTTP methods allowed by your API
                 .allowedHeaders("*"); // Add the allowed request headers
     }
-}
+}*/
